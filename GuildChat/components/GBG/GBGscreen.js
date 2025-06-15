bca359fe964c897dc1d3eae8421df34b4aef64ef
DmytroVaDimkaA75 <vadimkaa75@gmail.com>
2025-06-16 00:13:28 +0300

    Make group text non-interactive

--- GuildChat/components/GBG/GBGscreen.js
+++ GuildChat/components/GBG/GBGscreen.js
@@ -24,6 +24,7 @@ const GVG = () => {
         >
           <G id="gA5A" onPress={() => handleShapePress('A5A')}>
             <Polygon
+              id="A5A"
               points="50,93.397 13.397,73.397 13.397,33.397 50,13.397 86.603,33.397 86.603,73.397"
               fill="#0000ff"
               stroke="#ffffff"
@@ -32,6 +33,7 @@ const GVG = () => {
               transform="matrix(0,0.24307514,-0.24307514,0,82.247483,-3.0134026)"
             />
             <Text
+              id="tA5A"
               x={62.599854}
               y={11.622864}
               fontSize={7.05605}
@@ -39,6 +41,7 @@ const GVG = () => {
               fill="#000000"
               stroke="#131313"
               strokeWidth={0}
+              pointerEvents="none"
             >
               A5A
             </Text>
