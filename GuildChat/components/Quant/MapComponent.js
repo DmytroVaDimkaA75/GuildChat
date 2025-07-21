6f1c7fbcbb14cc779d5b7e3468d675578626c55e
DmytroVaDimkaA75 <vadimkaa75@gmail.com>
2025-07-21 22:16:26 +0300

    feat: set desktop user agent for WebView

--- GuildChat/components/Quant/MapComponent.js
+++ GuildChat/components/Quant/MapComponent.js
@@ -3,9 +3,15 @@ import { StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function MapComponent() {
+  const desktopUserAgent =
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36';
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: 'https://ru11.forgeofempires.com/' }} style={styles.webview} />
+      <WebView
+        source={{ uri: 'https://ru11.forgeofempires.com/' }}
+        style={styles.webview}
+        userAgent={desktopUserAgent}
+      />
     </View>
   );
 }
