d504351c8028c4d1e7fc6a950d8e6185fa766ee5
DmytroVaDimkaA75 <vadimkaa75@gmail.com>
2026-01-11 21:54:49 +0200

    Replace cache button with battles leaderboard

---
+++ android/app/src/main/java/com/vadimkaa75/guildchat/widgets/GbgWidgetRefreshService.kt
@@ -0,0 +1,17 @@
+package com.vadimkaa75.guildchat.widgets
+
+import android.content.Intent
+import com.facebook.react.HeadlessJsTaskService
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.jstasks.HeadlessJsTaskConfig
+
+class GbgWidgetRefreshService : HeadlessJsTaskService() {
+  override fun getTaskConfig(intent: Intent?): HeadlessJsTaskConfig? {
+    return HeadlessJsTaskConfig(
+      "GbgWidgetRefreshTask",
+      Arguments.createMap(),
+      60_000,
+      true
+    )
+  }
+}
